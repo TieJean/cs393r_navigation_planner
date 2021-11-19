4c369f946ee4bd66cc51034eb7b14f46af14af71
Taijing Chen <taijingchen99@gmail.com>
2021-11-19 13:19:22 -0600

    generate_data main example works

--- util/generate_data.py
+++ util/generate_data.py
@@ -1,6 +1,8 @@
 import os
 import in_place
 import subprocess
+import signal
+import time
 
 LUA_DIR = os.path.expanduser("~") + "/ut_automata/config/"
 # LUA_FILENAME = 'simulator.lua'
@@ -37,27 +39,62 @@ def change_lua(laser_noise_stddev, angular_drift_rate, angular_error_rate):
 # angular_error_rate_cutoff = 10.0  # absolute value
 
 def generate_bagfiles():
-    laser_noise_stddev = [0, 0.01, 0.02, 0.05, 0.07, 0.1]
-    angular_drift_rate = [0, 0.2, 0.3, 0.1, 1.5, 1.5]
-    angular_error_rate = [0, 5.0, 7.0, 25.0, 5.0, 25.0]
+    laser_noise_stddev = [0]
+    angular_drift_rate = [0]
+    angular_error_rate = [0]
+    # laser_noise_stddev = [0, 0.01, 0.02, 0.05, 0.07, 0.1]
+    # angular_drift_rate = [0, 0.2, 0.3, 0.1, 1.5, 1.5]
+    # angular_error_rate = [0, 5.0, 7.0, 25.0, 5.0, 25.0]
 
+    # os.chdir(UT_AUTOMATA_DIR)
+    # os.system(UT_AUTOMATA_DIR + "bin/simulator &")
     for laser_noise in laser_noise_stddev:
         for i in range(0, len(angular_drift_rate)):
             change_lua(laser_noise, angular_drift_rate[i], angular_error_rate[i])
-            # call rosbag play to record file
-            file_name = "laser" + laser_noise + "_drift" + angular_drift_rate[i] + "_error" + angular_error_rate[i]
-            os.chdir(UT_AUTOMATA_DIR)
-            os.system(UT_AUTOMATA_DIR + "/bin/simulator")
             os.chdir(PARTICLE_FILTER_DIR)
-            os.system("bin/navigation")
+            proc_nav = subprocess.Popen([PARTICLE_FILTER_DIR + "bin/navigation", "&"])
+            # proc_nav = subprocess.Popen("exec " + PARTICLE_FILTER_DIR + "bin/navigation &", shell=True)
+            os.chdir(UT_AUTOMATA_DIR)
+            proc_sim = subprocess.Popen([UT_AUTOMATA_DIR + "bin/simulator", "&"])
+            # proc_sim = subprocess.Popen("exec " + UT_AUTOMATA_DIR + "bin/simulator &", shell=True)
             os.chdir(PARTICLE_FILTER_DIR + "bag/")
-            os.system("rosbag record -a -O " + file_name + " --duration=15")
-            sleep(20)
+            file_name = "laser" + laser_noise + "_drift" + angular_drift_rate[i] + "_error" + angular_error_rate[i]
+            os.system("rosbag record -a -O " + file_name + " --duration=20 &")
+            # call rosbag play to record file
+            try:
+                outs, errs = proc_sim.communicate(timeout=15)
+                outs, errs = proc_nav.communicate(timeout=15)
+            except subprocess.TimeoutExpired:
+                proc_sim.kill()
+                proc_nav.kill()
+            time.sleep(25)
             
 
 if __name__ == '__main__':
-    PARTICLE_FILTER_DIR = str(os.chdir("../")) + "/"
+    os.chdir("../")
+    PARTICLE_FILTER_DIR = str(os.getcwd()) + "/"
+    # generate_bagfiles()
     # change_lua(1.0, 2.0, 3.0)
+    os.chdir(PARTICLE_FILTER_DIR)
+    proc_nav = subprocess.Popen([PARTICLE_FILTER_DIR + "bin/navigation", "&"])
     os.chdir(UT_AUTOMATA_DIR)
-    os.system("./bin/simulator")
+    proc_sim = subprocess.Popen([UT_AUTOMATA_DIR + "bin/simulator", "&"])
+    try:
+        outs, errs = proc_sim.communicate(timeout=1)
+        outs, errs = proc_nav.communicate(timeout=1)
+    except subprocess.TimeoutExpired:
+        print("before kill")
+        os.system("ps -aux | grep \"simulator\"")
+        os.system("ps -aux | grep \"navigation\"")
+        time.sleep(3)
+        proc_sim.kill()
+        proc_nav.kill()
+        print("after kill")
+        print("kill -9 " + str(proc_sim.pid), "kill -9 " + str(proc_nav.pid))
+        os.system("ps -aux | grep \"simulator\"")
+        os.system("ps -aux | grep \"navigation\"")
+    
+            
+
+
     
