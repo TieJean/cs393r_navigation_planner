e24e65f42effeea97ce01e6c0b581eab772abf08
Taijing Chen <taijingchen99@gmail.com>
2021-11-24 11:13:08 -0600

    add generate_pf_file.sh

---
+++ util/generate_pf_file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+prefix="pf_"
+
+cd ..
+PF_DIR=$(pwd)
+cd bag
+BAG_DIR=$(pwd)
+
+cd $BAG_DIR
+for f in *.bag
+do
+    rosbag filter $f tmp.bag "topic!='localization'"
+    sleep 1
+    (rosbag record localization odom scan -O $prefix$f --duration=30) &
+    cd $PF_DIR
+    (./bin/particle_filter) &
+    pf_pid=$!
+    cd $BAG_DIR
+    rosbag play tmp.bag
+    kill -9 $pf_pid
+    ps -aux | grep "particle_filter"
+    rm tmp.bag
+    sleep 1
+done
