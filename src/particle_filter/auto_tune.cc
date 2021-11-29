e0db51b378e0cad52cea3f810ca1974b60a5fc30
Taijing Chen <taijingchen99@gmail.com>
2021-11-29 12:24:51 -0600

    fix indentation in comment

--- src/particle_filter/auto_tune.cc
+++ src/particle_filter/auto_tune.cc
@@ -9,43 +9,43 @@ using namespace std;
 namespace auto_tune {
 
 AutoTune::AutoTune() {
-  LoadContexts_();
+	LoadContexts_();
 }
 
 void AutoTune::LoadContexts_() {
-  string line;
+	string line;
 	string context_name;
-  ifstream fp;
+	ifstream fp;
 	fp.open(BEST_PARAMS_FILE);
 	if (!fp.is_open()) {
 		printf("error in opening file\n");
 		exit(1);
 	}
-  while(getline(fp, line)) {
+	while(getline(fp, line)) {
 		stringstream tokens(line);
 		tokens >> context_name;
 		
 		Parameter param;
 		tokens >> param.sensor_std >> param.d_short >> param.d_long >> param.motion_dist_k1 >> param.motion_dist_k2 >> param.motion_a_k1 >> param.motion_a_k2;
-  
-    contexts.emplace_back(context_name, param);
-  }
-  fp.close();
+	
+		contexts.emplace_back(context_name, param);
+	}
+	fp.close();
 
-  // for (auto& context : contexts) {
-	// 	cout << context.name << endl;
-  //   cout << context.param.sensor_std << " ";
-  //   cout << context.param.d_short << " ";
-  //   cout << context.param.d_long << " ";
-  //   cout << context.param.motion_dist_k1 << " ";
-  //   cout << context.param.motion_dist_k2 << " ";
-  //   cout << context.param.motion_a_k1 << " ";
-  //   cout << context.param.motion_a_k2 << endl;
-	// }
+	for (auto& context : contexts) {
+		cout << context.name << endl;
+		cout << context.param.sensor_std << " ";
+		cout << context.param.d_short << " ";
+		cout << context.param.d_long << " ";
+		cout << context.param.motion_dist_k1 << " ";
+		cout << context.param.motion_dist_k2 << " ";
+		cout << context.param.motion_a_k1 << " ";
+		cout << context.param.motion_a_k2 << endl;
+	}
 }
 
 void AutoTune::DetectContext() {
-  // TODO
+	// TODO
 }
 
 
@@ -53,7 +53,7 @@ void AutoTune::DetectContext() {
 } // namespace auto_tune
 
 int main() {
-  cout << "Auto" << endl;
+	cout << "Auto" << endl;
 	auto_tune::AutoTune autoTune;
 	return 0;
 }
